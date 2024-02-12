efe7afecaa598e221f750560dc35c8fbadf10acb
alhasann351 <alhasann352@gmail.com>
2024-02-12 21:04:10 +0600

    customize modal bottom sheet added

--- lib/main.dart
+++ lib/main.dart
@@ -35,25 +35,262 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Theme.of(context).primaryColor,
       context: context,
       builder: (context) {
-        return const Padding(
-          padding: EdgeInsets.all(15),
+        return Padding(
+          padding: const EdgeInsets.all(15),
           child: Column(
             children: [
-              Row(
-                children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 60,
-                        height: 60,
-                        child: CircleAvatar(
-                          child: Icon(Icons.email_outlined, size: 40,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 60,
+                          height: 60,
+                          child: CircleAvatar(
+                            child: Icon(
+                              Icons.email_outlined,
+                              size: 40,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text(
+                            'Email',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 60,
+                          height: 60,
+                          child: CircleAvatar(
+                            child: Icon(
+                              Icons.message_outlined,
+                              size: 40,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text(
+                            'Message',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 60,
+                          height: 60,
+                          child: CircleAvatar(
+                            child: Icon(
+                              Icons.camera_alt_outlined,
+                              size: 40,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text(
+                            'Camera',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 60,
+                          height: 60,
+                          child: CircleAvatar(
+                            child: Icon(
+                              Icons.contact_page_outlined,
+                              size: 40,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text(
+                            'Contact',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 60,
+                          height: 60,
+                          child: CircleAvatar(
+                            child: Icon(
+                              Icons.share_outlined,
+                              size: 40,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text(
+                            'Share',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 60,
+                          height: 60,
+                          child: CircleAvatar(
+                            child: Icon(
+                              Icons.alarm_add_outlined,
+                              size: 40,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text(
+                            'Alarm',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 60,
+                          height: 60,
+                          child: CircleAvatar(
+                            child: Icon(
+                              Icons.call,
+                              size: 40,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text(
+                            'Call',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: 60,
+                          height: 60,
+                          child: CircleAvatar(
+                            child: Icon(
+                              Icons.person_outline,
+                              size: 40,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text(
+                            'Person',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(15),
+                child: Text(
+                  'This is Simple Modal Bottom Sheet in Flutter App.',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: SizedBox(
+                  height: 50,
+                  width: 200,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Center(
+                      child: Text(
+                        'Dismiss',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Text('Email', style: TextStyle(fontSize: 20, color: Colors.white,),),
-                    ],
+                    ),
                   ),
-                ],
+                ),
               ),
             ],
           ),
